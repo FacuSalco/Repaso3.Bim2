2358f7cc7b7a1344c7d31a626855950fa7636127
FacuSalco <facusalco26@gmail.com>
2022-05-20 13:45:39 -0300

    ButtonClonner

---
+++ Assets/PrefabCloner.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PrefabCloner : MonoBehaviour
+{
+
+    public GameObject prefab;
+
+    void Start()
+    {
+        
+    }
+
+    
+    void Update()
+    {
+        
+    }
+
+    public void ClonePrefab()
+    {
+        Instantiate(prefab);
+    }
+}
